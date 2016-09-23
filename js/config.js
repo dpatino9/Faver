a819aaa49d4b5cc01f4efeeaff0ad44cf2534ae9
aksm <albert.min@gmail.com>
2016-09-23 15:16:02 -0400

    Add firebase config file and update html. Move js files to bottom of body (for faster loading)

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCXFW-VVMWgCjS52nAy1tmhsGt7c-5qzRI",
+	authDomain: "teamproject1-e14a5.firebaseapp.com",
+	databaseURL: "https://teamproject1-e14a5.firebaseio.com",
+	storageBucket: "teamproject1-e14a5.appspot.com",
+	messagingSenderId: "487434650615"
+};
+firebase.initializeApp(config);
