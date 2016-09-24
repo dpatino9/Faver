a17160be15f5ec3ffc3a4d02e28afdb50643169c
Tatiana Marsh <tatiana.alcocer@gmail.com>
2016-09-24 15:15:05 -0400

    Modal functions and restrict access to modals until search input

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ $(document).ready(function(){
 
 
 
-
 //AJAX CALL FOR AMAZON: ===================================================================================
 
 
@@ -25,14 +24,52 @@ $(document).ready(function(){
 
 //FUNCTIONS: ==============================================================================================
 
+// Displays shop modal if user has input search and selected shop
+function displayModal(event){
+	if ($("#search").val().trim().length !== 0) {
+		var modal = event.currentTarget.id;
+
+		console.log("value: " + $("#search").val());
+		$(".direction-copy").removeClass("red-text");
+		$(".direction-copy").removeClass("error-anim");
+
+		openShopModal(modal);
+
+	} else {
+		console.log("empty search");
+		// $(".direction-copy").addClass("waves-effect");
+		$(".direction-copy").addClass("red-text");
+		$(".direction-copy").addClass("error-anim");
+	}
+}
+
+// General function for opening modals
+function openShopModal(id) {
+	switch(id) {
+		case 'ebay-toggle': 
+			$('#ebay-modal').openModal();
+			break;
+		case 'amazon-toggle':
+			$('#amazon-modal').openModal();
+			break;
+		default:
+			console.log('how did this happen!!');
+	}
+}
 
 
 //EVENT LISTENERS: ========================================================================================
 
+// Displays pop-over for username and password
 $('#login').webuiPopover({url:'#login-form'});
 
+// Opens modals on click
+$(".modal-trigger").on("click", displayModal);
 
-// bind to the submit event, then check to see they have selected a shop
+
+
+
+
 
 
 
