1e7a151cbdf0f5f196bd853940d7f3e7ca61cfd8
aksm <albert.min@gmail.com>
2016-09-30 21:04:40 -0400

    Incorporate working ebay carousel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
 
 //VARIABLES: ==============================================================================================
 
@@ -18,47 +17,68 @@ $(document).ready(function(){
 
 
 //EBAY: ====================================================================================================
-
+var ebayResults = "";
 // Callback for finding items by keyword, creating a table of results
-window._cb_findItemsByKeywords = function _cb_findItemsByKeywords(root) {
+// window._cb_findItemsByKeywords = 
+function ebayParse(root) {
+    console.log("ran");
   var items = root.findItemsByKeywordsResponse[0].searchResult[0].item || [];
-  var html = [];
-  html.push('<table width="100%" border="0" cellspacing="0" cellpadding="3"><tbody>');
+  // var html = [];
+  // html.push('<table width="100%" border="0" cellspacing="0" cellpadding="3"><tbody>');
+        $("#ebay-panel").empty();
+        var carousel = $("<div class='carousel'>");
   for (var i = 0; i < items.length; ++i) {
     var item     = items[i];
     var title    = item.title;
     var pic      = item.galleryURL;
     var viewitem = item.viewItemURL;
     if (null != title && null != viewitem) {
-      html.push('<tr><td>' + '<img src="' + pic + '" border="0">' + '</td>' + 
-      '<td><a href="' + viewitem + '" target="_blank">' + title + '</a></td></tr>');
+      // html.push('<tr><td>' + '<img src="' + pic + '" border="0">' + '</td>' + 
+      // '<td><a href="' + viewitem + '" target="_blank">' + title + '</a></td></tr>');
+            var carouselItem = $("<a class='carousel-item'><div class='card'>");
+            carouselItem.append("<div class='card-image'><a href='"+viewitem+"' target='_blank'><img class='responsive-img' src='"+pic+"'>")
+            carouselItem.append("<div class='card-content'><p>"+title+"<p>"+item);
+            carousel.append(carouselItem);
     }
   }
-  html.push('</tbody></table>');
-  document.getElementById("ebay-panel").innerHTML = html.join("");
+        $("#ebay-panel").append(carousel);
+        $('.carousel').carousel({
+            dist:0,
+            shift:0,
+            padding:20,
+        });
+  // html.push('</tbody></table>');
+  // document.getElementById("ebay-panel").innerHTML = html.join("");
 };
 
 
-function ebaySearch(searchTerm) {
+function ebaySearch(searchTerm, ebayParse) {
 	var urlfilter = "";
 	var keyword = searchTerm;
 
 	// Appending the API script to the document
-	var url = "http://svcs.ebay.com/services/search/FindingService/v1";
+	var url = "https://crossorigin.me/http://svcs.ebay.com/services/search/FindingService/v1";
 	    url += "?OPERATION-NAME=findItemsByKeywords";
 	    url += "&SERVICE-VERSION=1.0.0";
 	    url += "&SECURITY-APPNAME=DanielMi-projecto-PRD-c2f55bc58-2a0828cd";
 	    url += "&GLOBAL-ID=EBAY-US";
 	    url += "&RESPONSE-DATA-FORMAT=JSON";
-	    url += "&callback=_cb_findItemsByKeywords";
+	    // url += "&callback=_cb_findItemsByKeywords";
 	    url += "&REST-PAYLOAD";
 	    url += "&keywords=" + keyword;
 	    url += "&paginationInput.entriesPerPage=10";
 	    url += urlfilter;
 
-	    s=document.createElement('script'); // create script element
-	    s.src= url;
-	    document.body.appendChild(s);
+	    // s=document.createElement('script'); // create script element
+	    // s.src= url;
+	    // document.body.appendChild(s);
+            $.ajax({url: url, method: "GET"}).done(function(response) {
+                ebayResults = response;
+                console.log(response);
+                // var responseObject = JSON.parse(response);
+                // console.log(responseObject);
+                ebayParse(JSON.parse(response));
+        });
 }
 
 
@@ -95,8 +115,32 @@ function ebaySearch(searchTerm) {
         var time = h + ":" + min + ":" + s;
         return date + "T" + time + "Z";
     }
+    function displayData(data,i,j) {
+        $("#amazon-panel").empty();
+        var carousel = $("<div class='carousel'>");
+        for(i; i<j; i++) {
+            var carouselItem = $("<a class='carousel-item'><div class='card'>");
+            carouselItem.append("<div class='card-image'><a href='"+$(data).find("DetailPageURL").eq(i).text()+"' target='_blank'><img class='responsive-img' src='"+$(data).find("MediumImage URL").eq(i).text()+"'>")
+            carouselItem.append("<div class='card-content'><p>"+$(data).find("Title").eq(i).text()+"<p>"+$(data).find("FormattedPrice").eq(i).text())
+            carousel.append(carouselItem);
+        }
+        $("#amazon-panel").append(carousel);
+        $('.carousel').carousel({
+            dist:0,
+            shift:0,
+            padding:20,
+        });
+    }
+    function getData(amazonUrl, displayData) {
+        var queryURL = "https://crossorigin.me/"+amazonUrl;
+            $.ajax({url: queryURL, dataType: "xml", method: "GET"}).done(function(response) {
+                results = response;
+                displayData(results,i,j);
+        });
 
-    function getAmazonItemInfo(keyword) {
+    }
+
+    function getAmazonItemInfo(keyword, getData) {
         var PrivateKey = "/nmngLta6ifXG0RjOzoICN0cMqRlUp9m9LwlIrOj";
         var PublicKey = "AKIAIGNV6AJOIUYCURCA";
         var AssociateTag = "shopsmart093-20";
@@ -121,32 +165,9 @@ function ebaySearch(searchTerm) {
 
         amazonUrl =  "http://webservices.amazon.com/onca/xml?" + paramString + "&Signature=" + signature;
         console.log(amazonUrl);
+        getData(amazonUrl, displayData);
     }
-	function getData(amazonUrl) {
-		var queryURL = "https://crossorigin.me/"+amazonUrl;
-			$.ajax({url: queryURL, dataType: "xml", method: "GET"}).done(function(response) {
-                results = response;
-                displayData(results,i,j);
-		});
 
-	}
-
-    function displayData(data,i,j) {
-        $("#amazon-panel").empty();
-        var carousel = $("<div class='carousel'>");
-        for(i; i<j; i++) {
-        	var carouselItem = $("<a class='carousel-item'><div class='card'>");
-            carouselItem.append("<div class='card-image'><a href='"+$(data).find("DetailPageURL").eq(i).text()+"' target='_blank'><img src='"+$(data).find("MediumImage URL").eq(i).text()+"'>")
-            carouselItem.append("<div class='card-content'><p>"+$(data).find("Title").eq(i).text()+"<p>"+$(data).find("FormattedPrice").eq(i).text())
-            carousel.append(carouselItem);
-        }
-        $("#amazon-panel").append(carousel);
-        $('.carousel').carousel({
-            dist:0,
-            shift:0,
-            padding:20,
-      	});
-    }
 
 
 
@@ -215,6 +236,7 @@ function addFavorite(){
     // Add to the favoritesCount
     favoritesCount++;
 }
+$(document).ready(function(){
 
 
 //EVENT LISTENERS: ========================================================================================
@@ -245,10 +267,13 @@ $(document.body).on('click', '.close', function(){
 
 // });
 
-// $("#search-bar").on('submit', function() {
-// 	openDisplay("shop-panels");
-// 	$("#search").val("");
-// });
+$("#search-bar").on('submit', function() {
+	// openDisplay("shop-panels");
+	var searchterm = $("#search").val();
+    ebaySearch(searchterm, ebayParse);
+    getAmazonItemInfo(searchterm, getData);
+    $("#shop-panels").removeClass("hidden");
+});
 
 
 
